b0ca5fa6519b14cc8b76f6076e14c8e4c8b6588d
sudhakar4 <sudhakar4@gmail.com>
2015-11-02 11:04:33 +0530

    Test Project
    Test Project

---
+++ src/main/java/Test_project/Test_project/App.java
@@ -0,0 +1,13 @@
+package Test_project.Test_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Test_project/Test_project/Tstcase1.java
@@ -0,0 +1,10 @@
+package Test_project.Test_project;
+
+import org.testng.annotations.Test;
+
+public class Tstcase1 {
+@Test
+public void testcase_method(){
+	System.out.println("Method1");
+}
+}
